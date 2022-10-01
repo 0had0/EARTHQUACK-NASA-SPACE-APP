3747328d2cf9055b8a32d6710d0d221651ebcee2
0had0 <hadihoussainy75@gmail.com>
2022-10-01 13:41:40 +0300

    dynamic load image

--- src/screens/InteractiveGlob/InteractiveGlob.js
+++ src/screens/InteractiveGlob/InteractiveGlob.js
@@ -1,10 +1,14 @@
+/* eslint-disable */
+import React, {useContext} from 'react';
 import Globe from 'react-globe.gl';
 import * as d3 from 'd3';
 
 import moonLocations from '../../constants/moon_landings'
 import useForceUpdate from "./useForceUpdate";
-import {useContext} from "react";
 import {GlobContext} from "../../contexts/glob.context";
+import ProgressiveImage from "react-progressive-image";
+
+import placeholderImage from './lunar_surface_small.png'
 
 const colorScale = d3.scaleOrdinal(['orangered', 'mediumblue', 'darkgreen', 'yellow']);
 
@@ -12,26 +16,32 @@ const labelsTopOrientation = new Set(['Apollo 12', 'Luna 2', 'Luna 20', 'Luna 21
 
 function InteractiveGlob() {
     const {options} = useContext(GlobContext)
-  useForceUpdate();
+    useForceUpdate();
+
     return (
-        <Globe
-            bumpImageUrl={`${process.env.PUBLIC_URL}/lunar_bumpmap.png`}
-            globeImageUrl={`${process.env.PUBLIC_URL}/lunar_surface.png`}
-            showGraticules={true}
-            waitForGlobeReady={false}
-            labelsData={options[0].value ? JSON.parse(JSON.stringify(moonLocations)): []}
-            labelText="label"
-            labelSize={1.7}
-            labelDotRadius={0.4}
-            labelDotOrientation={d => labelsTopOrientation.has(d.label) ? 'top' : 'bottom'}
-            labelColor={d => colorScale(d.agency)}
-            labelLabel={d => {
-                return `<div><b>${d.label}</b></div>
+        <ProgressiveImage
+            src={'https://raw.githubusercontent.com/vasturiano/react-globe.gl/master/example/moon-landing-sites/lunar_surface.jpg'} placeholder={placeholderImage}>
+            {src => {
+                return <Globe
+                    bumpImageUrl={"https://raw.githubusercontent.com/vasturiano/react-globe.gl/master/example/moon-landing-sites/lunar_bumpmap.jpg"}
+                    globeImageUrl={src}
+                    showGraticules={true}
+                    waitForGlobeReady={true}
+                    labelsData={options[0].value ? JSON.parse(JSON.stringify(moonLocations)) : []}
+                    labelText="label"
+                    labelSize={1.7}
+                    labelDotRadius={0.4}
+                    labelDotOrientation={d => labelsTopOrientation.has(d.label) ? 'top' : 'bottom'}
+                    labelColor={d => colorScale(d.agency)}
+                    labelLabel={d => {
+                        return `<div><b>${d.label}</b></div>
                         <div>${d.agency} - ${d.program} Program</div>
                         <div>Landing on <i>${new Date(d.date).toLocaleDateString()}</i></div>`
+                    }}
+                    onLabelClick={d => window.open(d.url, '_blank')}
+                />
             }}
-            onLabelClick={d => window.open(d.url, '_blank')}
-        />
+        </ProgressiveImage>
     );
 }
 
